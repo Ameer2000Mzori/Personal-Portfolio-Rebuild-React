9c55a28db672199740c0bad6f18d960a423013f3
Ameer Ameen <ameerapex496@gmail.com>
2024-02-20 04:55:10 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -7,49 +7,7 @@ import Footer from './components/pages/Footer.jsx'
 function App() {
   return (
     <>
-      {/* <!-- animation button --> */}
-      <div className="open-Close-Btn">
-        <span className="bar-One"></span>
-        <span className="bar-Two"></span>
-      </div>
-
-      {/* <!-- navbar --> */}
-      <nav className="navbar active">
-        <div className="decoration-Bar">
-          <div className="cercle-One"></div>
-          <div className="line-El"></div>
-          <div className="cercle-Two"></div>
-        </div>
-        <div className="nav-Links">
-          <ul>
-            <li>
-              <a href="#">HOME</a>
-            </li>
-            <li>
-              <a href="#">ABOUT</a>
-            </li>
-            <li>
-              <a href="#">PROJECTS</a>
-            </li>
-            <li>
-              <a href="#">GALLERY</a>
-            </li>
-            <li>
-              <a href="#">BLOG</a>
-            </li>
-            <li>
-              <a href="#">CLIENTS</a>
-            </li>
-            <li>
-              <a href="#">PRICING</a>
-            </li>
-            <li>
-              <a href="#">CONTACT</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Navbar />
       {/* <!-- header --> */}
       <header className="header">
         <div className="header-Top-Wrap">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <>
+      {/* <!-- animation button --> */}
+      <div className="open-Close-Btn">
+        <span className="bar-One"></span>
+        <span className="bar-Two"></span>
+      </div>
+
+      {/* <!-- navbar --> */}
+      <nav className="navbar active">
+        <div className="decoration-Bar">
+          <div className="cercle-One"></div>
+          <div className="line-El"></div>
+          <div className="cercle-Two"></div>
+        </div>
+        <div className="nav-Links">
+          <ul>
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">ABOUT</a>
+            </li>
+            <li>
+              <a href="#">PROJECTS</a>
+            </li>
+            <li>
+              <a href="#">GALLERY</a>
+            </li>
+            <li>
+              <a href="#">BLOG</a>
+            </li>
+            <li>
+              <a href="#">CLIENTS</a>
+            </li>
+            <li>
+              <a href="#">PRICING</a>
+            </li>
+            <li>
+              <a href="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default NavBar
