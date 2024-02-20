20b4e86383edb29838200a96220358a0addc781b
Ameer Ameen <ameerapex496@gmail.com>
2024-02-20 05:49:48 +0100

    added all logic needed for navbar to work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavLinks from './NavLinks'
 const NavBar = () => {
+  const [navOpen, setNavOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen)
+  }
+
   return (
     <>
       {/* <!-- animation button --> */}
-      <div className="open-Close-Btn">
-        <span className="bar-One"></span>
-        <span className="bar-Two"></span>
+      <div onClick={toggleNav} className={`open-Close-Btn  `}>
+        <span className={`bar-One ${navOpen && 'active'} `}></span>
+        <span className={`bar-Two ${navOpen && 'active'} `}></span>
       </div>
 
       {/* <!-- navbar --> */}
-      <nav className="navbar ">
+      <nav className={`navbar  ${!navOpen && 'active'} `}>
         <div className="decoration-Bar">
           <div className="cercle-One"></div>
           <div className="line-El"></div>
