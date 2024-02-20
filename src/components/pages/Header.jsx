c7320ce7880df3a80b46f4744571974c7a798f26
Ameer Ameen <ameerapex496@gmail.com>
2024-02-20 04:56:31 +0100

    linked and replaced alot of html

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,7 +1,81 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <>
+      {/* <!-- header --> */}
+      <header className="header">
+        <div className="header-Top-Wrap">
+          <div className="logo">
+            <span className="logo-T-One">Web</span>
+            <span className="logo-T-Two">Dev</span>
+          </div>
+        </div>
+        <div className="header-Top-Mid">
+          <div className="left-banner">
+            <div className="cercle-One"></div>
+            <div className="line-El"></div>
+            <div className="cercle-Two"></div>
+          </div>
+          <div className="mid-banner">
+            <div className="information-Wrap">
+              <div className="title-Text">
+                <h3 className="title-Span-One">HELLO,</h3>
+                <h3 className="title-Span-Two">MY NAME IS</h3>
+              </div>
+              <div className="title-Name">
+                <h1 className="first-Name">Ameer</h1>
+                <h1 className="Second-Name">Ameen</h1>
+              </div>
+              <h3 className="text-Two">I AM Web Developer</h3>
+              <p className="text-Par">
+                🌟 Ameer | 23 | Netherlands | Full Stack Developer | Exploring
+                Opportunities in React, Node.js, MongoDB | Open to New Ventures
+                🚀
+              </p>
+              <div className="social-Buttons">
+                <a
+                  href="https://github.com/Ameer2000Mzori"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/ameer-ameen-82314425b/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+              </div>
+              <div className="cv-Btn-Down">
+                <button className="down-Btn">DOWNLOAD CV</button>
+                <div className="line-Decoration"></div>
+                <h4 className="text-Btn-Last">MY SKILLS</h4>
+              </div>
+            </div>
+          </div>
+          <div className="right-Banner">
+            <div className="img-Text-Circle"></div>
+            <div className="text-Cont-1">
+              <h3>0.5</h3>
+              <h4>YEARS OF EXPERINCE</h4>
+            </div>
+            <div className="text-Cont-2">
+              <h3>80+</h3>
+              <h4>COMPELETED PROJECTS</h4>
+            </div>
+          </div>
+        </div>
+        <div className="header-Top-Bottom">
+          <h1 className="bottom-Text">
+            <i>Web Developer</i>
+          </h1>
+        </div>
+      </header>
+    </>
+  )
 }
 
 export default Header
--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,7 +1,143 @@
 import React from 'react'
 
 const Skills = () => {
-  return <div>Skills</div>
+  return (
+    <>
+      {/* <!--  Skills section --> */}
+      <section className="skills">
+        <div className="left-Deco-Skills">
+          <div className="skills-Cercle-One"></div>
+          <div className="skills-Line-El"></div>
+          <div className="skills-Cercle-Two"></div>
+        </div>
+        <div className="right-Skill-Skills">
+          <div className="titles-Skills">
+            <h1 className="skills-Text-One">PROFESSIONAL SKILLS</h1>
+            <div className="skills-Text-Two">
+              <h3 className="text-Skills-2-1">MY</h3>
+              <h3 className="text-Skills-2-2">Skills</h3>
+            </div>
+          </div>
+          <div className="skills-Cards-Wrap">
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">HTML</h2>
+                <h2 className="skills-Num">95%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill">
+                    <div className="skills-Range-Circile"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">CSS</h2>
+                <h2 className="skills-Num">85%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill Css">
+                    <div className="skills-Range-Circile Css"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">SCSS</h2>
+                <h2 className="skills-Num">60%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill Scss">
+                    <div className="skills-Range-Circile Scss"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">JAVASCRIPT</h2>
+                <h2 className="skills-Num">55%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill JavaScript">
+                    <div className="skills-Range-Circile JavaScript"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">TYPESCRIPT</h2>
+                <h2 className="skills-Num">35%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill TypeScript">
+                    <div className="skills-Range-Circile TypeScript"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="skills-Card">
+              <div className="name-Lang-Num">
+                <h2 className="skills-Name">TOOLS</h2>
+                <h2 className="skills-Num">50%</h2>
+              </div>
+              <p className="skills-P">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit facere, nobis nisi, blanditiis laborum, cum
+                eveniet id quas consequatur modi in pariatur error est hic
+                voluptates quos beatae amet tempora?
+              </p>
+              <div className="skills-Range-Wrap">
+                <div className="skills-Range">
+                  <div className="skills-Range-Fill Tools">
+                    <div className="skills-Range-Circile Tools"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Skills
