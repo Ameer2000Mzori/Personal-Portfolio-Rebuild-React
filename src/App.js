b2c12c10ac0b76baeea658a550b817bd55fa0691
Ameer Ameen <ameerapex496@gmail.com>
2024-02-20 04:54:11 +0100

    imported all files needed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import react from 'react'
+import Navbar from './components/NavBar.jsx'
+import Header from './components/pages/Header.jsx'
+import Skills from './components/pages/Skills.jsx'
+import Projects from './components/pages/Projects.jsx'
+import Footer from './components/pages/Footer.jsx'
 function App() {
   return (
     <>
